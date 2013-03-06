fa2c19b21b8645793b2a5097f9d139fd590b3f32
Rob Poort <robpoort@gmail.com>
2013-03-06 10:46:14 +0100

    nieuwe voorbeelddata

--- j25.sql
+++ j25.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: localhost
--- Genereertijd: 25 feb 2013 om 16:49
+-- Genereertijd: 06 mrt 2013 om 10:44
 -- Serverversie: 5.5.16
 -- PHP-Versie: 5.3.8
 
@@ -51,8 +51,8 @@ CREATE TABLE IF NOT EXISTS `localtest_jexbooking_arrangements` (
 
 INSERT INTO `localtest_jexbooking_arrangements` (`id`, `location_id`, `type_id`, `name`, `desc`, `start_date`, `end_date`, `nights`, `price`, `extra_pp`, `is_pp`, `use_extra_pp`, `is_pa`, `required`, `published`) VALUES
 (1, 6, 6, 'diamantsnip normaal', 'diamantsnip normaal tarief midweek', '18-01-2013', '25-01-2013', 5, 650.75, 51.25, 0, 0, 1, 0, 1),
-(2, 4, 6, 'standaardplaats voorjaarsvoordeel', 'sp voorjaars arrangement', '15-02-2013', '25-02-2013', 0, 805.30, 75.50, 1, 1, 0, 1, 1),
-(3, 4, 6, 'Frisse, fruitige lente maand', '', '04-05-2013', '04-06-2013', 0, 765.80, 0.00, 0, 0, 1, 1, 1),
+(2, 4, 6, 'standaardplaats voorjaarsvoordeel', 'sp voorjaars arrangement', '10-03-2013', '26-03-2013', 0, 805.30, 75.50, 1, 1, 0, 1, 1),
+(3, 5, 6, 'Frisse, fruitige lente maand', '', '04-05-2013', '04-06-2013', 0, 765.80, 0.00, 0, 0, 1, 1, 1),
 (4, 5, 6, 'hooikiep juni', '', '04-06-2013', '18-06-2013', 0, 500.00, 0.00, 1, 0, 1, 0, 1);
 
 -- --------------------------------------------------------
@@ -117,18 +117,22 @@ CREATE TABLE IF NOT EXISTS `localtest_jexbooking_default_prices` (
   `is_pn_min_price` int(1) NOT NULL DEFAULT '1',
   `extra` float(10,2) NOT NULL,
   `is_pn_extra` int(1) NOT NULL DEFAULT '1',
+  `is_default` int(1) NOT NULL DEFAULT '0',
   `published` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `localtest_jexbooking_default_prices`
 --
 
-INSERT INTO `localtest_jexbooking_default_prices` (`id`, `location_id`, `name`, `start_date`, `end_date`, `min_price`, `is_pn_min_price`, `extra`, `is_pn_extra`, `published`) VALUES
-(1, 4, 'sp voorjaarstarief', '16-01-2013', '12-05-2013', 5.50, 1, 3.50, 1, 1),
-(2, 4, 'sp hoogseizoen', '13-05-2013', '09-09-2013', 9.50, 1, 3.75, 1, 1),
-(6, 7, 'zonnezicht januari', '11-01-2013', '31-01-2013', 0.00, 1, 0.00, 1, 1);
+INSERT INTO `localtest_jexbooking_default_prices` (`id`, `location_id`, `name`, `start_date`, `end_date`, `min_price`, `is_pn_min_price`, `extra`, `is_pn_extra`, `is_default`, `published`) VALUES
+(1, 4, 'sp voorjaarstarief', '16-01-2013', '12-05-2013', 5.50, 1, 3.50, 1, 0, 1),
+(2, 4, 'sp hoogseizoen', '12-05-2013', '20-06-2013', 9.50, 1, 3.75, 1, 0, 1),
+(6, 7, 'zonnezicht januari', '11-01-2013', '31-01-2013', 0.00, 1, 0.00, 1, 0, 1),
+(7, 4, 'sp American Summer', '20-06-2013', '01-08-2013', 6.00, 1, 4.00, 1, 0, 1),
+(8, 4, 'sp defaults', '01-02-2013', '28-02-2014', 5.00, 1, 0.00, 1, 1, 1),
+(9, 4, 'sp naseizoen', '01-08-2013', '12-09-2013', 6.00, 1, 4.00, 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -212,7 +216,7 @@ CREATE TABLE IF NOT EXISTS `localtest_jexbooking_xref_attributes` (
   `default_id` int(11) NOT NULL,
   `arr_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=361 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=444 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `localtest_jexbooking_xref_attributes`
@@ -226,8 +230,8 @@ INSERT INTO `localtest_jexbooking_xref_attributes` (`id`, `attribute_id`, `type_
 (20, 5, 6, 0, 0, 0),
 (21, 7, 6, 0, 0, 0),
 (172, 1, 0, 0, 0, 4),
-(289, 6, 0, 0, 0, 3),
-(288, 4, 0, 0, 0, 3),
+(373, 6, 0, 0, 0, 3),
+(372, 4, 0, 0, 0, 3),
 (62, 5, 0, 6, 0, 0),
 (61, 3, 0, 6, 0, 0),
 (329, 11, 0, 4, 0, 0),
@@ -238,16 +242,16 @@ INSERT INTO `localtest_jexbooking_xref_attributes` (`id`, `attribute_id`, `type_
 (72, 1, 0, 5, 0, 0),
 (60, 1, 0, 6, 0, 0),
 (63, 7, 0, 6, 0, 0),
-(360, 11, 0, 0, 0, 2),
-(359, 9, 0, 0, 0, 2),
+(443, 11, 0, 0, 0, 2),
+(442, 9, 0, 0, 0, 2),
 (340, 9, 0, 0, 0, 1),
 (339, 8, 0, 0, 0, 1),
-(358, 10, 0, 0, 0, 2),
-(357, 8, 0, 0, 0, 2),
-(356, 6, 0, 0, 0, 2),
-(355, 5, 0, 0, 0, 2),
-(354, 4, 0, 0, 0, 2),
-(287, 3, 0, 0, 0, 3),
+(441, 10, 0, 0, 0, 2),
+(440, 8, 0, 0, 0, 2),
+(439, 6, 0, 0, 0, 2),
+(438, 5, 0, 0, 0, 2),
+(437, 4, 0, 0, 0, 2),
+(371, 3, 0, 0, 0, 3),
 (173, 2, 0, 0, 0, 4),
 (174, 3, 0, 0, 0, 4),
 (175, 4, 0, 0, 0, 4),
@@ -257,9 +261,9 @@ INSERT INTO `localtest_jexbooking_xref_attributes` (`id`, `attribute_id`, `type_
 (338, 6, 0, 0, 0, 1),
 (337, 5, 0, 0, 0, 1),
 (336, 4, 0, 0, 0, 1),
-(353, 3, 0, 0, 0, 2),
-(352, 2, 0, 0, 0, 2),
-(351, 1, 0, 0, 0, 2),
+(436, 3, 0, 0, 0, 2),
+(435, 2, 0, 0, 0, 2),
+(434, 1, 0, 0, 0, 2),
 (325, 6, 0, 4, 0, 0),
 (324, 5, 0, 4, 0, 0),
 (323, 4, 0, 4, 0, 0),
